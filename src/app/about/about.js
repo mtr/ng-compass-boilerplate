451d808f3fdb8b8abc2b5fa2c3ea0e49e3741a4e
Martin Thorsen Ranang <mtr@ranang.org>
2014-08-27 23:50:03 +0200

    Added missing module.

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -1,24 +1,23 @@
-angular.module( 'ngBoilerplate.about', [
-  'ui.bootstrap',
-  'titleService'
+angular.module('ngBoilerplate.about', [
+    'ngRoute',
+    'ui.bootstrap',
+    'titleService'
 ])
 
-.config(function config( $routeProvider ) {
-  $routeProvider.when( '/about', {
-    controller: 'AboutCtrl',
-    templateUrl: 'about/about.tpl.html'
-  });
-})
+    .config(function config($routeProvider) {
+        $routeProvider.when('/about', {
+            controller: 'AboutCtrl',
+            templateUrl: 'about/about.tpl.html'
+        });
+    })
 
-.controller( 'AboutCtrl', function AboutCtrl( $scope, titleService ) {
-  titleService.setTitle( 'What is It?' );
-  
-  // This is simple a demo for UI Boostrap.
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
-})
+    .controller('AboutCtrl', function AboutCtrl($scope, titleService) {
+        titleService.setTitle('What is It?');
 
-;
+        // This is simple a demo for UI Boostrap.
+        $scope.dropdownDemoItems = [
+            "The first choice!",
+            "And another choice for you.",
+            "but wait! A third!"
+        ];
+    });

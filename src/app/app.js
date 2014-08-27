e3b8b94447940f8d38c1d14c418dd8ec7d795e9b
Martin Thorsen Ranang <mtr@ranang.org>
2014-08-27 23:42:17 +0200

    Corrected Angular path so that correct version is used during build.  As a consequence, had to add the separated angular-route and angular-mocks packages.

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,22 @@
-angular.module( 'ngBoilerplate', [
-  'app-templates',
-  'component-templates',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
-  'ui.route'
+angular.module('ngBoilerplate', [
+    'ngRoute',
+    'app-templates',
+    'component-templates',
+    'ngBoilerplate.home',
+    'ngBoilerplate.about',
+    'ui.route'
 ])
 
-.config( function myAppConfig ( $routeProvider ) {
-  $routeProvider.otherwise({ redirectTo: '/home' });
-})
+    .config(function myAppConfig($routeProvider) {
+        $routeProvider.otherwise({ redirectTo: '/home' });
+    })
 
-.run( function run ( titleService ) {
-  titleService.setSuffix( ' | ngBoilerplate' );
-})
+    .run(function run(titleService) {
+        titleService.setSuffix(' | ngBoilerplate');
+    })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-})
+    .controller('AppCtrl', function AppCtrl($scope, $location) {
+    })
 
 ;
 
